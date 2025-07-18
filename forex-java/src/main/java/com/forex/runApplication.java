50ab053ad4ceb3bce5dfc931b9f7d37e2de13c58
Hui Zhen <1751079822@qq.com>
2025-07-18 08:28:06 +0000

    a springboot frame

---
+++ forex-java/src/main/java/com/forex/runApplication.java
@@ -0,0 +1,12 @@
+package com.forex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class runApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(runApplication.class, args);
+    }
+}
+
